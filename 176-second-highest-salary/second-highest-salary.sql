f4ca81aad4e06dc13f6895e4d1181e9cd2517037
Aronjohn20 <aronjohn12112004@gmail.com>
2026-05-30 17:57:02 +0300

    Time: 303 ms (22.19%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,14 +1,12 @@
--- with ct AS
--- (
--- SELECT id,salary,
--- DENSE_RANK() OVER(ORDER BY salary DESC) as ranking
--- FROM employee
--- )
--- SELECT DISTINCT COALESCE(
---     (SELECT salary FROM ct WHERE ranking = 2), 
---     NULL
--- ) AS SecondHighestSalary;
+select 
+( select distinct salary from(
+SELECT id,salary,
+DENSE_RANK() OVER(ORDER BY salary DESC) as ranking
+FROM employee
+) as ranked
+where ranking = 2
+) AS SecondHighestSalary;
 
-SELECT MAX(salary) AS SecondHighestSalary
-FROM employee WHERE salary < 
-(SELECT MAX(salary) FROM Employee);
+-- SELECT MAX(salary) AS SecondHighestSalary
+-- FROM employee WHERE salary < 
+-- (SELECT MAX(salary) FROM Employee);
