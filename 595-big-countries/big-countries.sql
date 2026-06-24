2c1dab2e5612ef8bd8b2771bfc126d7b4ff39e66
Aronjohn20 <aronjohn12112004@gmail.com>
2026-06-25 00:55:26 +0530

    Time: 282 ms (93.60%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,3 +1,5 @@
-SELECT name,area,population 
-FROM world
-WHERE area>=3000000 OR population>=25000000;
+# Write your MySQL query statement below
+SELECT name,population,area FROM
+World
+where area>=3000000
+OR population >=25000000
